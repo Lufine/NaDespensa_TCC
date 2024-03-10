ec6630328f9b09de492780189768cb25eb261075
Lufine <lfneuwirth@gmail.com>
2024-03-09 22:54:41 -0300

    NaDespensa

--- NaDespensa/NaDespensa/urls.py
+++ NaDespensa/NaDespensa/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('auth/register/', views.register, name='register'),
     path('auth/login/', views.login, name='login'),
     path('auth/logout/', views.logout, name='logout'),
-    path('auth/produtos/', views.produtos, name='produtos'),
+    path('produtos/', views.produtos, name='produtos'),
 
 ]
